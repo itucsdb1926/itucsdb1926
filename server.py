56eff1bb2473c5e6f14eb0d551a5ecd0ce8d9aac
gulizarkondel <cal.gulizz@gmail.com>
2019-11-10 23:22:11 +0300

    login added

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from wtform_fields import *
 
-
+#Configure app
 app = Flask(__name__)
+app.secret_key = 'replace later'
 
+@app.route("/", methods = ['GET', 'POST'])
+def login():
+	reg_form = RegistrationForm()
+	return render_template("login_layout.html",form = reg_form)
 
-@app.route("/")
-def home_page():
-    return "Hello, world!"
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__== "__main__":
+	app.run(debug=True)
