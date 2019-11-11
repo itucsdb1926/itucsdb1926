656e388aeb4b104ec7ea3fce9ce01fd514eab48d
gulizarkondel <cal.gulizz@gmail.com>
2019-11-11 14:53:37 +0300

    login added

--- wtform_fields.py
+++ wtform_fields.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField,SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo
 
 
 class RegistrationForm(FlaskForm):	
-	username = StringField('username', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
-	password = PasswordField('password', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
+	username = StringField('username', validators=[InputRequired(message="Username required")])
+	password = PasswordField('password', validators=[InputRequired(message="Password required")])
 	confirm_pswd = PasswordField('confirm_pswd', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
-	
+	submit_button = SubmitField('Login')
