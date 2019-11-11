95ad91822f1093b038d2cb121b5280aa9a3b9698
gulizarkondel <cal.gulizz@gmail.com>
2019-11-11 14:58:42 +0300

    registration page added

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ app.secret_key = 'replace later'
 @app.route("/", methods = ['GET', 'POST'])
 def login():
 	reg_form = RegistrationForm()
+		if reg_form.validate_on_submit():
+		return "Succesfully registered"
 	return render_template("login_layout.html",form = reg_form)
 
 if __name__== "__main__":
